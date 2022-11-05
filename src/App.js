13ce91c123c3689534c7d88edf77435289d829b5
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2022-11-05 00:13:51 +0000

    Making a new mui react todo app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { useEffect, useState } from "react";
+
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import TaskCard from "./components/TaskCard";
 
 function App() {
+  const lightTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#0057b7",
+      },
+    },
+  });
+
+  const [taskList, setTaskList] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:8000/tasks/')
+  .then((response) => response.json())
+  .then((data) => setTaskList(data));
+  }, [])
+  
+
+  const displayTasks = ()=>{
+    return taskList.map((task) => (
+      <TaskCard
+        task={task}
+      />
+    ));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline />
+      <Box sx={{ flexGrow: 1, minWidth: 1 }} key="1">
+        <Grid container spacing={0}>
+          <Grid item xs={12}>
+            
+          </Grid>
+          {displayTasks()}
+        </Grid>
+      </Box>
+          
+      
+    </ThemeProvider>
   );
 }
 
